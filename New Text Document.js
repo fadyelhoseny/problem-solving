67d8c417898094033cb874b26d551e0c32c83fe2
fadyelhoseny <fadyelgmal@gmail.com>
2024-08-12 00:12:45 +0300

    Add files via upload
    mm

---
+++ New Text Document.js
@@ -0,0 +1,88 @@
+//1
+var v=+prompt("Enter number")
+if (v>=18) {
+    alert("yes")
+}
+else if (18>v) {
+    alert("no")
+}
+//2
+var char=+prompt("Enter number")
+if (char<=100) {
+    alert("short")
+}
+else if (100<char) {
+    alert("long")
+}
+//3
+
+var b=+prompt("enterNumber")
+
+if (b !==7777) {
+    alert("nonValid")
+}
+else if (b= 7777 ) {
+alert("Validation")
+}
+//4
+var week=7;
+n=1;
+console.log(week-n );
+m=2;
+console.log(week-m );
+
+//5
+var v=+prompt("Enter number")
+if (v>0) {
+    alert("postive")
+}
+else if (0>v) {
+    alert("negative")
+}
+//6
+var grade=+prompt("Enter number")
+if (grade<50) {
+    alert("f")
+}
+else if (grade>60&&grade<70) {
+    alert("D")
+}
+else if (grade>70&&grade<80) {
+    alert("c")
+}
+    else if (grade>80 &&grade<90) {
+        alert("B")
+    }
+    else if (90<grade) {
+        alert("A") 
+    }
+    //7
+    var temp=+prompt("enterNumber")
+
+if (temp>30) {
+    alert("hot")
+}
+else if (temp<30 ) {
+alert("cold")
+}
+//8
+let number = 5
+let reminder = (number % 2)
+if ( reminder == 0 ) 
+    {
+    console.log('${number} is even');
+} else{
+    console.log('${number} is odd');
+
+}
+     //9
+   
+   let degree=+prompt("enterNumber>20");
+   let remi = (degree % 2)
+   if ( remi == 0 ) 
+       {
+       console.log('${degree} is even');
+   } else{
+       console.log('${degree} is odd');
+   
+   }
